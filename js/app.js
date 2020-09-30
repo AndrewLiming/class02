4121039bf68a836c44d5cc9f9f03aa5b5ed9676c
Andrew Liming <andrewliming206@gmail.com>
2020-09-30 14:28:04 -0700

    update to javascript

--- js/app.js
+++ js/app.js
@@ -1,12 +1,14 @@
 'use strict'
+var counter = 0
 
 var userName = prompt('Please enter your name');
-var greeting = 'Welcome to Andrews about me ' + userName; 
-  document.write(greeting);
+var greeting = 'Welcome to Andrews about me ' + userName;
+document.write(greeting);
 
-var andrewKitchen=prompt('Has Andrew worked in kitchens?');
+var andrewKitchen = prompt('Has Andrew worked in kitchens?');
 
-if(andrewKitchen.toLowerCase() === 'yes' || andrewKitchen.toLowerCase() === 'y'){
+if (andrewKitchen.toLowerCase() === 'yes' || andrewKitchen.toLowerCase() === 'y') {
+    counter++
     alert('you are correct!');
     // console.log('you are correct')
 } else {
@@ -14,9 +16,10 @@ if(andrewKitchen.toLowerCase() === 'yes' || andrewKitchen.toLowerCase() === 'y')
     // console.log('Andrew has worked in kitchens');
 }
 
-var andrewSober=prompt('Is Andrew in Recovery?');
+var andrewSober = prompt('Is Andrew in Recovery?');
 
-if(andrewSober.toLowerCase() === 'yes' || andrewSober.toLowerCase() === 'y'){
+if (andrewSober.toLowerCase() === 'yes' || andrewSober.toLowerCase() === 'y') {
+    counter++
     alert('you are right!');
     // console.log('you are right!');
 } else {
@@ -24,9 +27,10 @@ if(andrewSober.toLowerCase() === 'yes' || andrewSober.toLowerCase() === 'y'){
     // console.log('Andrew has over 5 years sober');
 }
 
-var andrewInstrument=prompt('Can Andrew play a musical instrument?');
+var andrewInstrument = prompt('Can Andrew play a musical instrument?');
 
-if(andrewInstrument.toLowerCase() === 'yes' || andrewInstrument.toLowerCase() === 'y'){
+if (andrewInstrument.toLowerCase() === 'yes' || andrewInstrument.toLowerCase() === 'y') {
+    counter++
     alert('Yes, he can play 5 instruments!')
     // console.log('Yes, he can play 5 instruments!');
 } else {
@@ -34,9 +38,10 @@ if(andrewInstrument.toLowerCase() === 'yes' || andrewInstrument.toLowerCase() ==
     // console.log('Andrew has been playing instruments since the age of 7 and can play 5 of them');
 }
 
-var andrewDnd=prompt('Does Andrew play Dungeons and Dragons?');
+var andrewDnd = prompt('Does Andrew play Dungeons and Dragons?');
 
-if(andrewDnd.toLowerCase() === 'yes' || andrewDnd.toLowerCase() === 'y'){
+if (andrewDnd.toLowerCase() === 'yes' || andrewDnd.toLowerCase() === 'y') {
+    counter++
     alert('Yep! Every Thursday');
     // console.log('Yep! Every Thursday');
 } else {
@@ -44,12 +49,58 @@ if(andrewDnd.toLowerCase() === 'yes' || andrewDnd.toLowerCase() === 'y'){
     // console log('Sorry, It's one of his favorite hobbies');
 }
 
-var andrewHorror=prompt('Does Andrew like Horror movies?');
+var andrewHorror = prompt('Does Andrew like Horror movies?');
 
-if(andrewHorror.toLowerCase() === 'yes' || andrewHorror.toLowerCase() === 'y'){
+if (andrewHorror.toLowerCase() === 'yes' || andrewHorror.toLowerCase() === 'y') {
+    counter++
     alert('you are correct,' + ' ' + userName)
     // console.log('you are correct,' + ' ' + userName);
 } else {
     alert('Andrew loves horror movies' + ' ' + userName);
     // console.log('Andrew loves horror movies' + ' ' + userName);
 }
+var answer = '7';
+
+for (var i = 1; i <= 4; i++) {
+    var userGuess = prompt('Pick a number between 1 and 10');
+    if (userGuess === answer) {
+        counter++
+        alert('You are correct!');
+        break;
+    }
+    else {
+        if (userGuess < answer) {
+            alert('Too Low');
+        }
+        else {
+            alert('Too High');
+
+        }
+        if (i === 4) {
+            alert('Better luck next time')
+        }
+    }
+
+}
+
+
+
+var favoriteFoods = ['Risotto', 'Duck', 'Tagliatelle', 'Gnocchi', 'Ribeye Steak', 'Crab', 'Brick Oven Pizza', 'Gumbo', 'Jambalaya', 'Cheesecake'];
+
+for (var i = 1; i <= 6; i++) {
+    var userGuess = prompt('What is one of Andrews favorite food to cook?');
+    if (userGuess === favoriteFoods[i]) {
+        counter++
+        alert('You are right!');
+        break
+    }
+    else {
+        if (userGuess !== favoriteFoods[i]){
+            alert('Try Again');
+        }
+        if (i === 6) {
+            alert('Sorry, the correct answers were Risotto, Duck, Tagliatelle, Gnocchi, Ribeye Steak, Crab, Brick Oven Pizza, Gumbo, Jambalaya, and Cheesecake')
+        }
+    }
+}
+alert('congrats ' + userName + ' you got ' + counter + ' correct!')
